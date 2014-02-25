80ffd4f6ef7740947c3f2af8f2e5b93e76eb4c0c
Pete Dunlap <petedunlap@gmail.com>
2014-02-25 11:26:50 -0600

    updated styling for digital droppings

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,7 @@ exports.index = function(req, res){
   res.render('home/index', {title: 'Pete Dunlap'});
 };
 
-
 // GET /digital_droppings
 exports.digital_droppings = function(req, res){
-  res.render('digital_droppings/digital_droppings', {title: 'Pete Dunlap'});
+  res.render('digital_droppings/digital_droppings', {title: 'Digital Droppings'});
 };
